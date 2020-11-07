012c67d2aaf585eaf1aa8fcb6dbbbbba7b6ab464
dmorka <morkadawid@gmail.com>
2020-11-07 19:02:22 +0100

    Design movie table in MoviesTable.vue.

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,20 @@
   <b-container>
     <h1>Baza filmów</h1>
     <SearchEngine></SearchEngine>
+    <MoviesTable></MoviesTable>
     <MovieByGenreList></MovieByGenreList>
   </b-container>
 </template>
 
 <script>
-// import MoviesTable from "@/components/MoviesTable";
+import MoviesTable from "@/components/MoviesTable";
 import SearchEngine from "@/components/SearchEngine";
 import MovieByGenreList from "@/components/MoviesByGenreList";
 
 export default {
   name: 'App',
   components: {
-    // MoviesTable,
+    MoviesTable,
     SearchEngine,
     MovieByGenreList
   }
--- src/components/MoviesTable.vue
+++ src/components/MoviesTable.vue
@@ -1,9 +1,41 @@
 <template>
+  <div>
+    <b-table small :fields="fields" :items="items" responsive="sm">
+      <template #cell(title)="data">
+        {{data.item.title}}
+      </template>
+      <template #cell(year)="data">
+        {{data.item.year}}
+      </template>
+      <template #cell(cast)="data">
+        <span v-for="person in data.item.cast" v-bind:key="person">
+          {{person+', '}}
+        </span>
+      </template>
+      <template #cell(genres)="data">
+        <span v-for="genre in data.item.genres" v-bind:key="genre">
+          {{genre+', '}}
+        </span>
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
+import movies from "../assets/movies/movies.json"
 export default {
-  name: "MoviesTable"
+  name: "MoviesTable",
+  data() {
+    return {
+      fields: [
+        'title',
+        { key: 'year', label: 'Production Year' },
+        'cast',
+        'genres'
+      ],
+      items: movies
+    }
+  }
 }
 </script>
 
