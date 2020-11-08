c7e25b289f1063f854e74dde15ba4f8587c07687
dmorka <morkadawid@gmail.com>
2020-11-08 12:44:46 +0100

    Design MoviesByGenreList.vue in MoviesByGenreList.vue, added genres.json.

--- src/components/MoviesByGenreList.vue
+++ src/components/MoviesByGenreList.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
     <h1>Filmy wg gatunku</h1>
-    <div v-for="item in result" v-bind:key="item">{{item.title}}</div>
+    <div v-for="item in moviesByGenre" v-bind:key="item">
+      <div v-if="item.movies.length !==0">
+        <h2>{{item.genre}}</h2>
+        <ol>
+          <li v-for="movie in item.movies" v-bind:key="movie">
+            {{movie.title}}
+          </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import movies from '../assets/movies/movies.json'
-// import genres from '../assets/movies/genres.json'
+import genres from '../assets/movies/genres.json'
 
 export default{
   data(){
     let _ = require('underscore');
-    let data = movies
-    let result = _.first(_.reject(data,
-        function(item){ return (item.genres.length === 0);}), 10);
-    return{
-      result
+    let moviesByGenre = _.map(genres, function (genre){
+      return {genre: genre, movies: _.filter(movies, function (movie){
+        return _.contains(movie.genres, genre)
+      })}
+    })
+    return {
+      movies,
+      genres,
+      moviesByGenre: moviesByGenre,
     }
   }
 }
